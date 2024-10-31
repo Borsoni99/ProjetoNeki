db5c442e53f133a8063d3fe181f0a6771a76191c
Borsoni <40211721+Borsoni99@users.noreply.github.com>
2024-10-30 23:29:45 -0300

    Update main.py

--- PSNeki/main.py
+++ PSNeki/main.py
@@ -10,42 +10,34 @@ from langchain_openai.chat_models import ChatOpenAI
 os.environ["OPENAI_API_KEY"] = 'API_KEY'
 
 def splitar_documentos(docs):
-    # Inicializa o divisor de texto
     splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-    # Divide os documentos
     splitted = splitter.split_documents(docs)
     return splitted
 
 def criar_indice(textos):
-    # Cria embeddings usando o modelo 'text-embedding-ada-002'
     embd = OpenAIEmbeddings()
     embd.model = "text-embedding-ada-002"
-    # Cria um índice FAISS
     idx = FAISS.from_documents(textos, embd)
     return idx
 
 def criar_chain(indice):
-    # Inicializa o modelo de chatbot da OpenAI
     chatbot = ChatOpenAI()
-    chatbot.model_name = "gpt-3.5-turbo" # Define o modelo a ser usado
-    chatbot.temperature = 0  # Define a temperatura
+    chatbot.model_name = "gpt-3.5-turbo" 
+    chatbot.temperature = 0  
     # Cria uma cadeia de QA
     chain = RetrievalQA.from_chain_type(llm=chatbot, chain_type="stuff", retriever=indice.as_retriever())
     return chain
 
 if __name__ == "__main__":
-    # Define o local onde os PDFs estão armazenados
     diretorio_pdfs = './arquivos'
 
     # Lista todos os arquivos no local especificado
     arquivos = os.listdir(diretorio_pdfs)
     docs = []
-    # Para cada arquivo, carrega o conteúdo usando o PyPDFLoader
     for arquivo in arquivos:
         caminho = diretorio_pdfs + '/' + arquivo
         loader = PyPDFLoader(caminho)
         data = loader.load()
-        # Adiciona cada documento carregado à lista de documentos
         for d in data:
             docs.append(d)
 
